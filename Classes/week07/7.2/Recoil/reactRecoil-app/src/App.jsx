a616c3319648061977bb2755296423052c2413a0
Aapush01 <mdshahidafridia31@gmail.com>
2024-05-16 17:46:57 +0530

    use tsparticles.js

---
+++ Classes/week07/7.2/Recoil/reactRecoil-app/src/App.jsx
@@ -0,0 +1,15 @@
+ import React from 'react'
+ import './App.css'
+import ParticlesComponent from './components/ParticlesComponent'
+ 
+ const App = () => {
+   return (
+     <div>
+      <ParticlesComponent/>
+      
+      <h1>Bismillah</h1>
+     </div>
+   )
+ }
+ 
+ export default App
