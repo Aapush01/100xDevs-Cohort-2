073623379a472ec514ace7fb41d09bad8284b5dd
Aapush01 <mdshahidafridia31@gmail.com>
2024-05-18 11:51:28 +0530

    Atom

---
+++ Classes/week07/7.3/Atom-family/reactApp/src/App.jsx
@@ -0,0 +1,24 @@
+ 
+import './App.css'
+
+function App() {
+   
+
+  return (
+    <>
+       <div className="container">
+       <button>Home</button>
+       <button>My Networks()</button>
+       <button>Jobs()</button>
+       <button>Messaging()</button>
+       <button>Notification()</button>
+       <button>Me</button>
+       </div>
+        <div className="about">
+          <p>Here I will talk about Atom and selectors in Recoil</p>
+        </div>
+    </>
+  )
+}
+
+export default App
