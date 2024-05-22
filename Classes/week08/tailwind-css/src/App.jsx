60f72e42edf936bb38f59d9a1dba31e4ca1a220e
Aapush01 <mdshahidafridia31@gmail.com>
2024-05-22 11:59:40 +0530

    Tailwind install

--- Classes/week08/tailwind-css/src/App.jsx
+++ Classes/week08/tailwind-css/src/App.jsx
@@ -6,9 +6,7 @@
      {/* here try to write a code using a raw CSS and then try to write same code using tailwind. */}
       {/* here I write inline css  */}
      <div style={{display: "flex", justifyContent: "center" , fontSize: "30px" }} >
-     <div style={{backgroundColor: "pink"}}>MD</div>
-     <div style={{backgroundColor: "red"}}>Shahid</div>
-     <div style={{backgroundColor: "yellow"}}>Afridi</div>
+      <h1>Now lets start making project using tailwind</h1>
 
      </div> 
      </>
