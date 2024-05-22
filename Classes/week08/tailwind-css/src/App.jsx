f1af34cf50ff8a7ffbbf91bb791da79242d1b701
Aapush01 <mdshahidafridia31@gmail.com>
2024-05-22 12:39:51 +0530

    tailwind use

--- Classes/week08/tailwind-css/src/App.jsx
+++ Classes/week08/tailwind-css/src/App.jsx
@@ -5,10 +5,19 @@
      <>
      {/* here try to write a code using a raw CSS and then try to write same code using tailwind. */}
       {/* here I write inline css  */}
-     <div style={{display: "flex", justifyContent: "center" , fontSize: "30px" }} >
-      <h1>Now lets start making project using tailwind</h1>
+     <div style={{display: "flex", justifyContent: "space-around" , fontSize: "30px" }} >
+     <div style={{backgroundColor: "pink"}}>MD</div>
+     <div style={{backgroundColor: "red"}}>Shahid</div>
+     <div style={{backgroundColor: "yellow"}}>Afridi</div>
 
      </div> 
+     <b>Here I use tailwind</b>
+     <div className='flex justify-between' >
+     <div className='bg-white' >MD</div>
+     <div className='bg-zinc-800 text-white' >Shahid</div>
+     <div className='bg-slate-50' >Afridi</div>
+
+     </div>
      </>
    )
  }
