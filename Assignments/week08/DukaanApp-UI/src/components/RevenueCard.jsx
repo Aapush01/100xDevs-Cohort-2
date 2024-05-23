07b8ee2bc884392181f89739757618884174cc28
Aapush01 <mdshahidafridia31@gmail.com>
2024-05-23 13:54:12 +0530

    first component

--- Assignments/week08/DukaanApp-UI/src/components/RevenueCard.jsx
+++ Assignments/week08/DukaanApp-UI/src/components/RevenueCard.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export const RevenueCard = ({ title, orderCount, amount }) => {
   return (
-    <div className="bg-white rounded shadow-md p-4">
-      <div className="text-gray-700 justify-center ">
+    <div className="bg-white rounded shadow-md p-4 ">
+      <div className="flex text-gray-700 gap-1">
         <div>{title}</div>
         <div>
           <svg
@@ -21,6 +21,20 @@ export const RevenueCard = ({ title, orderCount, amount }) => {
             />
           </svg>
         </div>
+      </div>
+      <div className="flex gap-8">
+        <div className="flex"> <div ><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="2 0 22 22" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15 8.25H9m6 3H9m3 6-3-3h1.5a3 3 0 1 0 0-6M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+</svg>
+</div><div className="text-2xl font-semibold">{amount}</div></div>
+        <div className="flex text-blue-500 ">
+        <div className="underline">{orderCount} orders</div>
+            <div><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+</svg>
+</div>
+        </div>
+         
       </div>
     </div>
   );
