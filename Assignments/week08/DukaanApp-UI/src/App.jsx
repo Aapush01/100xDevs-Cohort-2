531f4f9d867caade87ba70f8381decf59743cc37
Aapush01 <mdshahidafridia31@gmail.com>
2024-05-25 13:12:08 +0530

    Formated

--- Assignments/week08/DukaanApp-UI/src/App.jsx
+++ Assignments/week08/DukaanApp-UI/src/App.jsx
@@ -1,13 +1,11 @@
-import Fotter from "./components/Fotter"
-import Overview from "./components/Overview"
-import Sidebar from "./components/Sidebar"
-import Table from "./components/Table"
-import Topbar from "./components/Topbar"
-import Transactions from "./components/Transactions"
-
+import Fotter from "./components/Fotter";
+import Overview from "./components/Overview";
+import Sidebar from "./components/Sidebar";
+import Table from "./components/Table";
+import Topbar from "./components/Topbar";
+import Transactions from "./components/Transactions";
 
 function App() {
-
   return (
     <div className="bg-white-50">
       <Sidebar />
@@ -15,18 +13,17 @@ function App() {
       <div className="border-b border-black-150 sm:pl-64 bg-white-500">
         <Topbar />
       </div>
-      
+
       <div className=" m-5 mb-0 sm:ml-64 grid gap-8">
         <Overview />
         <div className="grid gap-6">
           <Transactions />
           <Table />
         </div>
-        <Fotter/>
+        <Fotter />
       </div>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- Assignments/week08/DukaanApp-UI/src/components/Fotter.jsx
+++ Assignments/week08/DukaanApp-UI/src/components/Fotter.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React from "react";
 
 function Fotter() {
-    return (
-        <div className='flex justify-center items-center flex-col text-black-300 mb-6 gap-2'>
-            <div>
-                ----- Made By<a className='pl-1 text-blue-700 font-medium' href="https://twitter.com/Aapush2">MD. Shahid Afridi</a>, inspired by<a className='px-1 text-blue-700 font-medium' href="https://twitter.com/subhashchy/status/1744308069751025894?t=MrLV-PSnfsgv0Hg2jTz5JA&s=08">Dukaan App UI</a>-----
-            </div>
-            <div  className='text-black-300 underline' ><a href="https://github.com/Aapush01/100xDevs-Cohort-2/tree/main/Assignments/week08/DukaanApp-UI">Get the source code</a></div>
-        </div>
-    )
+  return (
+    <div className="flex justify-center items-center flex-col text-black-300 mb-6 gap-2">
+      <div>
+        ----- Made By
+        <a
+          className="pl-1 text-blue-700 font-medium"
+          href="https://twitter.com/Aapush2"
+        >
+          MD. Shahid Afridi
+        </a>
+        , inspired by
+        <a
+          className="px-1 text-blue-700 font-medium"
+          href="https://twitter.com/subhashchy/status/1744308069751025894?t=MrLV-PSnfsgv0Hg2jTz5JA&s=08"
+        >
+          Dukaan App UI
+        </a>
+        -----
+      </div>
+      <div className="text-black-300 underline">
+        <a href="https://github.com/Aapush01/100xDevs-Cohort-2/tree/main/Assignments/week08/DukaanApp-UI">
+          Get the source code
+        </a>
+      </div>
+    </div>
+  );
 }
 
-export default Fotter
+export default Fotter;
