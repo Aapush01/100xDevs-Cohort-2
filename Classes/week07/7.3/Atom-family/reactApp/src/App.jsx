e8122ca7891298c71e6c7d396f136c3ce0d7547a
Aapush01 <mdshahidafridia31@gmail.com>
2024-06-01 16:55:08 +0530

    update

--- Classes/week07/7.3/Atom-family/reactApp/src/App.jsx
+++ Classes/week07/7.3/Atom-family/reactApp/src/App.jsx
@@ -1,4 +1,4 @@
- 
+
 import { RecoilRoot, useRecoilValue } from 'recoil'
 import './App.css'
 import { jobsAtom, messagingAtom, networkAtom, notificationkAtom } from './atomsInRecoil'
@@ -6,34 +6,34 @@ import { jobsAtom, messagingAtom, networkAtom, notificationkAtom } from './atoms
 function App() {
   //  const notificationCount = useRecoilValue(notificationkAtom)
   return <RecoilRoot>
-    <MainApp/>
+    <MainApp />
   </RecoilRoot>
-   
+
 
 }
 
-function MainApp(){
-    const notificationCount = useRecoilValue(notificationkAtom);
-    const messagingCount = useRecoilValue(messagingAtom);
-    const jobsCount = useRecoilValue(jobsAtom);
-    const networkCount = useRecoilValue(networkAtom);
-     
-   
+function MainApp() {
+  const notificationCount = useRecoilValue(notificationkAtom);
+  const messagingCount = useRecoilValue(messagingAtom);
+  const jobsCount = useRecoilValue(jobsAtom);
+  const networkCount = useRecoilValue(networkAtom);
 
-   return (
-    
+
+
+  return (
+
     <>
-       <div className="container">
-       <button>Home</button>
-       <button>My Networks({networkCount >= 100 ? "99+" : networkCount})</button>
-       <button>Jobs({jobsCount})</button>
-       <button>Messaging({messagingCount})</button>
-       <button>Notification({notificationCount})</button>
-       <button>Me</button>
-       </div>
-        <div className="about">
-          <p>Here I will learn about Atom and selectors in Recoil</p>
-        </div>
+      <div className="container">
+        <button>Home</button>
+        <button>My Networks({networkCount >= 100 ? "99+" : networkCount})</button>
+        <button>Jobs({jobsCount})</button>
+        <button>Messaging({messagingCount})</button>
+        <button>Notification({notificationCount})</button>
+        <button>Me</button>
+      </div>
+      <div className="about">
+        <p>Here I will learn about Atom and selectors in Recoil</p>
+      </div>
     </>
   )
 }
